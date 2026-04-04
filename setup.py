22359e60c496cf9ee9bcfbb5de2da04384766843
QILING ZHANG <122155450+xsdgm@users.noreply.github.com>
2026-04-04 19:16:42 +0800

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Installation script for the 'unitree_rl_mjlab' python package."""
+
+from setuptools import setup, find_packages
+
+# Minimum dependencies required prior to installation
+INSTALL_REQUIRES = [
+    "mjlab==1.2.0",
+]
+
+# Installation operation
+setup(
+    name="unitree_rl_mjlab",
+    packages=["src"],
+    version="0.0.1",
+    install_requires=INSTALL_REQUIRES,
+)
